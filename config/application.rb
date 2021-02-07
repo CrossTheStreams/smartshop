9f9b8031498d808bfb9004d5780c1b8e51c3346a
Andrew Hautau <arhautau@gmail.com>
2021-02-06 18:09:15 -0800

    use a structure.sql

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module Smartshop
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
+    config.active_record.schema_format = :sql
 
     # Configuration for the application, engines, and railties goes here.
     #
